5c23c313959af581990016f7fdc8c2cf225feac2
Raphael Schweikert <any@sabberworm.com>
2020-04-04 14:00:15 +0200

    Add wcm.io dependency to gradle classpath, which breaks package validation

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("io.wcm.tooling.commons:io.wcm.tooling.commons.content-package-builder:1.4.4")
+}
